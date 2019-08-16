b10a07d6aed16a919e2652dbc5c5b3ad5e836c97
Nicolas Decat <nicolas.decat@gmail.com>
2019-08-16 16:43:19 +0200

    test

--- pages/index.vue
+++ pages/index.vue
@@ -122,6 +122,42 @@
     </section>
     <hr>
   </b-container>
+  <b-navbar toggleable="lg" type="dark" class="navbar">
+    <b-navbar-brand href="#">
+      <img
+        src="../assets/img/logo.jpg"
+        alt="Blue Note"
+        width="60"
+        height="60"
+        class="d-inline-block align-top"
+      >
+    </b-navbar-brand>
+
+    <b-navbar-toggle target="nav_collapse" />
+
+    <b-collapse id="nav_collapse" is-nav>
+      <b-navbar-nav class="mx-auto">
+        <b-nav-item
+          v-scroll-to="'#suggestion'"
+          href="#suggestion"
+        >
+          Suggestions
+        </b-nav-item>
+        <b-nav-item
+          v-scroll-to="'#partenaire'"
+          href="#partenaire"
+        >
+          Partenaires
+        </b-nav-item>
+        <b-nav-item href="#">
+          Réservations
+        </b-nav-item>
+        <b-nav-item href="#">
+          Contact
+        </b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
 </template>
 
 <script>
